c80672958f5ba8e8034ca2fdabc49d23e3103eb7
Vinhle1804 <131293438+Vinhle1804@users.noreply.github.com>
2025-02-27 17:01:15 +0700

    test slide session

--- src/components/slide-session.tsx
+++ src/components/slide-session.tsx
@@ -7,19 +7,25 @@ import { clientSessionToken } from "@/lib/http";
 
 export function SlideSession() {
   useEffect(() => {
+
+
+
     const interval = setInterval(async () => {
       const now = new Date();
       const expiresAt = new Date(clientSessionToken.expiresAt);
+      console.log("⏳ Kiểm tra session:", { now, expiresAt });
 
       if (differenceInHours(expiresAt, now) < 1) {
+        console.log("🔄 Session sắp hết hạn, gọi API refresh...");
         try {
           const res = await authApiRequest.slideSessionFromNextClientToNextServer();
           clientSessionToken.expiresAt = res?.payload?.data?.expiresAt;
+          console.log("✅ Session mới:", clientSessionToken.expiresAt);
         } catch (error) {
           console.error("Lỗi khi gọi slideSession:", error);
         }
       }
-    }, 1000 * 60 * 60); // Chạy mỗi 1 giờ
+    }, 60*1000); // Chạy mỗi 1 giờ
 
     return () => clearInterval(interval);
   }, []);
